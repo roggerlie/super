6ebc37cad78d0177918d844760b849947384a519
roggerlie <nirwananshor@gmail.com>
2021-07-15 10:16:55 +0700

    150721

--- config/elfinder.php
+++ config/elfinder.php
@@ -10,7 +10,7 @@ return array(
     | The dir where to store the images (relative from public)
     |
     */
-    'dir' => ['files'],
+    'dir' => ['storage'],
 
     /*
     |--------------------------------------------------------------------------
@@ -62,50 +62,7 @@ return array(
     |
     */
 
-    'roots' => array(
-        array(
-            'driver'        => 'LocalFileSystem', // driver for accessing file system (REQUIRED)
-            'path'          => public_path() . '/storage', // path to files (REQUIRED)
-            // 'URL'           => public_path() . '/storage',
-            'alias'         => 'Data Assets', // The name to replace your actual path name. (OPTIONAL)
-            'accessControl' => 'access',
-            'attributes' => array(
-                array(  // example for lock all .jpg files
-                    'pattern' => '/.+/',
-                    'read'   => true,
-                    'write'  => false,
-                    'locked' => false,
-                    'hidden' => false
-                ),
-                // array(    // example for lock all dir and file
-                //     'pattern' => '/.+/',
-                //     'read'   => true,
-                //     'write'  => false,
-                //     'locked' => false,
-                //     'hidden' => false
-                // ),
-                // array(    // example for lock single folder
-                //     'pattern'   => '!^/images!',
-                //     'read'      => true,
-                //     'write'     => false,
-                //     'locked'    => false,
-                //     'hidden'    => false
-                // ),
-                // array(  // example for lock all .jpg files
-                //     'pattern'   => '/.jpg+/',
-                //     'locked'    => true
-                // ),
-                // array(  // example for lock all .png files
-                //     'pattern'   => '/.png+/',
-                //     'locked'    => true
-                // ),
-                // array(  // example for lock all .gif files
-                //     'pattern'   => '/.gif+/',
-                //     'locked'    => true
-                // ),
-            )
-        )
-    ),
+    'roots' => null,
 
     /*
     |--------------------------------------------------------------------------
